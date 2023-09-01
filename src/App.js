1543bf4a3c86dc4a7dec8c39e18438b455ffe884
muhtor <weboasis9002@gmail.com>
2023-09-01 11:56:56 +0500

    task complated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import './App.css';  // Импортируем CSS-файл
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [offers, setOffers] = useState([]);
+
+    useEffect(() => {
+        // Выполните запрос к API при загрузке компонента
+        axios.get("https://www.kattabozor.uz/hh/test/api/v1/offers")
+            .then((response) => {
+                setOffers(response.data['offers']);
+            })
+            .catch((error) => {
+                console.error("Ошибка при получении данных:", error);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Electronics\Mobile phones</h1>
+            <div className="card-container">
+                {offers.map((offer) => (
+                    <div className="card" key={offer.id}>
+                        <h2>{offer.name}</h2>
+                        <img src={offer.image.url} alt={offer.name}/>
+                        <div className="card-text">
+                            <p>Brand: {offer.brand}</p>
+                            <p>Category: {offer.category}</p>
+                            <p>Merchant: {offer.merchant}</p>
+                            <p>RAM: {offer.attributes.find((attr) => attr.name === "RAM")?.value}</p>
+                            <p>ROM: {offer.attributes.find((attr) => attr.name === "ROM")?.value}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
